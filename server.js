169f5530295701baf51a1315130783518af8de84
Patricia Martinez <trish.on.a.mish@gmail.com>
2018-05-05 11:30:16 -0700

    updating stuff

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.engine("handlebars", exphbs({
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
+var routes = require("./controllers/controller-model.js");
 
 app.use(routes);
 
