340b1a79dd230d59581dbc3014ec4b0b1ffed590
Patricia Martinez <trish.on.a.mish@gmail.com>
2018-05-12 12:38:04 -0700

    book handlebar update

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO lifecoach_db.tasks
+;
+
+VALUES
+("Leg Day! WOOH!"),
+("Back and Abs! GET THAT ROCK HARD CORE"),
+("Chest and Arms! THESE ARE YOUR CHARMS"),
+("Walk! SWING THOSE HIPS"),
+("Run! FEEL THE BURN"),
+("Hike! WATCH OUT FOR SNAKES"),
+("Bike! WATCH FOR CARS"),
+("Yoga! BREATHE")
