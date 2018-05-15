c1e5c410624e82a87bc7e097aa695cb503d4fa1d
Patricia Martinez <trish.on.a.mish@gmail.com>
2018-05-15 15:09:57 -0700

    updating

--- controllers/controller-affirmations.js
+++ controllers/controller-affirmations.js
@@ -2,25 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 
-<<<<<<< HEAD
 router.get("/affirmations", function (req, res) {
-=======
-function isLoggedIn(req, res, next) {
-
-    if (req.isAuthenticated())
-
-        return next();
-
-    res.redirect('/signin');
-
-}
-
-// *** Routes
-// =============================================================
-
-// Request to find and display all exercise tasks (complete/incomplete).
-router.get("/affirmations", isLoggedIn, function (req, res) {
->>>>>>> 748f81eba406c729ccfa40b41390bd70b0b4e9a8
 
     res.render("affirmations");
 });
